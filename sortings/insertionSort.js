0ce1678e3ed4aea1917310994c1cad5aa9a9c212
karthik-rao-au3 <karthikb786@gmail.com>
2020-02-19 23:37:55 +0530

    insertion sort is updated

---
+++ sortings/insertionSort.js
@@ -0,0 +1,21 @@
+"use strict";
+"use strict";
+const insertionSort = arr => {
+  /*
+    Time complexity-O(n)
+    Space complexity - linear
+    */
+  for (let i = 1, n = arr.length; i < n; i++) {
+    let temp = arr[i];
+    let j = i;
+    while (j > 0 && temp < arr[j - 1]) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = temp;
+  }
+  return arr;
+};
+
+const arr = [3, 1, 5, 10, 7, 22, 2];
+console.log(insertionSort(arr));
